fb314b6d5280137565f0eac7ed6f7f148cc5418e
BluthardtTim <tim.bluthardt@hfg.design>
2025-05-18 19:03:23 +0200

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
     import Navbar from "$lib/components/Navbar.svelte";
     import "../app.css";
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
