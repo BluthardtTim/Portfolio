574d23ac7069cd14b4bf5d448ecee6d106e66f28
BluthardtTim <tim.bluthardt@hfg.design>
2025-05-18 19:07:40 +0200

    speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,11 @@
     import "../app.css";
     import { dev } from '$app/environment';
     import { injectAnalytics } from '@vercel/analytics/sveltekit';
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
  
+    
     injectAnalytics({ mode: dev ? 'development' : 'production' });
+    injectSpeedInsights();
 </script>
 
 <main>
